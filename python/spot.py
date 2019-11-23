021870a81ef6dfe2f546227b13f7bdb5d13b6702
Kai Lawrence <mail@kailawrence.de>
2019-11-23 17:52:17 +0100

    Use JSONs to authenticate spot

--- python/spot.py
+++ python/spot.py
@@ -1,18 +1,30 @@
 import asyncio
 import discord
+import json as json
 
 class Spot(discord.Client):
     '''
     import spot
-    with spot(id, token) as spot:
+    with spot('./config.json') as spot:
         spot.message('i love you')
     '''
-    def __init__(self, user_id, token, loop=asyncio.new_event_loop(), *args, **kwargs):
+    def __init__(self, file=None, id=None, token=None,
+        loop=asyncio.new_event_loop(), *args, **kwargs):
+        
+        if file is not None:
+            with open(file) as config:
+                auth = json.load(config)
+                id = auth['id']
+                token = auth['token']
+        elif id is None and token is None:
+            print('spot can not authenticate.')
+            return
+
         self.loop = loop
         super().__init__(*args, **kwargs)
         async def login(token):
             await self.login(token)
-            user = await self.fetch_user(user_id)
+            user = await self.fetch_user(id)
             self.channel = await user.create_dm()
             await self.channel.send('On duty!')
         self.loop.run_until_complete(login(token))
