4f9fc4a8b62c926f61b5b3e127123584761ede50
Kai Lawrence <mail@kailawrence.de>
2021-03-27 10:50:18 +0100

    Fix formatting

--- python/spot.py
+++ python/spot.py
@@ -1,31 +1,43 @@
 import asyncio
-import discord
 import json as json
 
+import discord
+
+
 class Spot(discord.Client):
-    '''
+    """
     import spot
     with spot('./config.json') as spot:
         spot.message('i love you')
-    '''
-    def __init__(self, file=None, id=None, token=None,
-        loop=asyncio.new_event_loop(), *args, **kwargs):
-        
+    """
+
+    def __init__(
+        self,
+        file=None,
+        id=None,
+        token=None,
+        loop=asyncio.new_event_loop(),
+        *args,
+        **kwargs
+    ):
+
         if file is not None:
             with open(file) as config:
                 auth = json.load(config)
-                id = auth['id']
-                token = auth['token']
+                id = auth["id"]
+                token = auth["token"]
         elif id is None and token is None:
-            print('spot can not authenticate.')
+            print("spot can not authenticate.")
             return
 
         self.loop = loop
         super().__init__(*args, **kwargs)
+
         async def login(token):
             await self.login(token)
             user = await self.fetch_user(id)
             self.channel = await user.create_dm()
+
         self.loop.run_until_complete(login(token))
 
     def __enter__(self):
@@ -34,10 +46,11 @@ class Spot(discord.Client):
     def exit(self):
         async def goodbye():
             await self.logout()
+
         self.loop.run_until_complete(goodbye())
 
     def __exit__(self, *args):
         self.exit()
-    
+
     def message(self, text):
         self.loop.run_until_complete(self.channel.send(text))
